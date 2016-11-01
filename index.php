f72fc88cad1a82e54201428bd3485636835f07c7
Myles <i@myles.hk>
2016-11-01 15:08:03 +0800

    Table redesign

--- index.php
+++ index.php
@@ -35,19 +35,17 @@
             <table width="100%" id="tableA" class="table-hover table-inputs">
                 <thead>
                 <tr class="options">
-                    <td rowspan="2">
+                    <td colspan="2">
                         <label for="startDate">Start date:</label>
-                        <input type="text" class="form-control" data-provide="datepicker" data-date-format="dd/mm/yyyy"
-                               data-date-autoclose="true" id="startDate" readonly>
                     </td>
-                    <th class="text-right">Money Rates</th>
+
                     <td colspan="2" class="text-center">
                         <select class="money red a form-control">
                             <option>act/360</option>
                             <option>act/365</option>
                         </select>
                     </td>
-                    <td class="separator"></td>
+                    <th class="separator">Money Rates</th>
                     <td colspan="2" class="text-center">
                         <select class="money blue b form-control">
                             <option>act/360</option>
@@ -57,7 +55,11 @@
 
                 </tr>
                 <tr class="options">
-                    <th class="text-right">Swap Rates</th>
+                    <td colspan="2">
+                        <input type="text" class="form-control" data-provide="datepicker" data-date-format="dd/mm/yyyy"
+                               data-date-autoclose="true" id="startDate" readonly>
+                    </td>
+
                     <td colspan="2" class="text-center">
                         <select class="swap red a form-control">
                             <option>30/360</option>
@@ -65,7 +67,7 @@
                             <option>act/365</option>
                         </select>
                     </td>
-                    <td class="separator"></td>
+                    <th class="separator">Swap Rates</th>
                     <td colspan="2" class="text-center">
                         <select class="swap blue b form-control">
                             <option>30/360</option>
@@ -78,7 +80,7 @@
                     <td colspan="6"></td>
                 </tr>
                 <tr>
-                    <th>t</th>
+                    <th class="t">t</th>
                     <th class="text-center">#days</th>
                     <th class="text-right red">Rate %</th>
                     <th class="text-right red">Discount Factor</th>
